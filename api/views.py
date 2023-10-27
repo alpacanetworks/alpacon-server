9a6c5d86856983c5c4621fbbea8e4fc36420f0f9
Eunyoung Jeong <ey.jeong@alpacanetworks.com>
2023-10-28 00:53:03 +0900

    Initialize api module

---
+++ api/views.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.reverse import reverse
+from rest_framework.response import Response
+from rest_framework import permissions
+
+
+@api_view(['GET'])
+@permission_classes((permissions.IsAuthenticated, ))
+def api_index(request, format=None):
+    api = {}
+    for app in settings.REST_API_APPS:
+        api[app] = reverse('api:%s:api-root' % app, request=request, format=format)
+    if 'api.api_auth' in settings.INSTALLED_APPS:
+        api['auth'] = reverse('api:auth:api-root', request=request, format=format)
+    api['status'] = reverse('api:status', request=request, format=format)
+    return Response(api)
+
+
+@api_view(['GET'])
+@permission_classes((permissions.AllowAny,))
+def status(request, format=None):
+    return Response({'status': 'Good'})
