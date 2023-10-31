9b68ad19f09b91bd7b886eacf78f4eef4cb79605
hyemin hong <hm.hong@alpacanetworks.com>
2023-10-31 13:57:32 +0900

    Add jwtauthentication, jwttokens at apiclient

--- api/apiclient/auth.py
+++ api/apiclient/auth.py
@@ -3,9 +3,14 @@ import logging
 
 from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from django.utils.translation import gettext_lazy as _
+from django.conf import settings
 
 from rest_framework.authentication import BaseAuthentication
 from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.request import Request
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.tokens import Token
+from rest_framework_simplejwt.exceptions import InvalidToken
 
 from api.apiclient.models import APIClient, APIClientUser
 
@@ -51,3 +56,38 @@ class APIClientAuthentication(BaseAuthentication):
         except Exception as e:
             logger.exception(e)
             return None
+
+class APIClientJWTAuthentication(JWTAuthentication):
+    """
+    An authentication plugin that authenticates requests through a JSON web token provided in a request header.
+    """
+
+    client_model = APIClient
+
+    def authenticate(self, request: Request):
+        header = self.get_header(request)
+        if header is None:
+            return None
+
+        raw_token = self.get_raw_token(header)
+        if raw_token is None:
+            return None
+
+        validated_token = self.get_validated_token(raw_token)
+        request.client = self.get_client(validated_token)
+        return (APIClientUser(request.client), validated_token)
+    
+    def get_client(self, validated_token: Token):
+        """
+        Attempts to find and return a client using the given validated token.
+        """
+        
+        try:
+            client_id = validated_token[settings.SIMPLE_JWT['CLIENT_ID_CLAIM']]
+        except KeyError:
+            raise InvalidToken(_('Token contained no recognizable user identification'))
+        try:
+            client = self.client_model.objects.get(**{settings.SIMPLE_JWT['CLIENT_ID_FIELD']: client_id})
+        except self.client_model.DoesNotExist:
+            raise AuthenticationFailed(_('Client not found'), code='client_not_found')
+        return client
